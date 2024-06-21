5e6d172a901c7f7185cc2f033ffd49f7dea53a38
Alexander Kazachinsky <alexander.kazachinsky@yandex.ru>
2024-06-21 15:39:19 +0300

    Check comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,17 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
 const cardsContainer = document.querySelector('.places__list');
 
-function createCard(item) {
+function createCard(item, handleDelete) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
-    cardItem.querySelector('.card__image').src=item.link;
-    cardItem.querySelector('.card__image').alt=`Изображение места: ${item.name}`;
-    cardItem.querySelector('.card__title').textContent=item.name;
-    cardItem.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-    cardsContainer.append(cardItem)
+    const cardImage = cardItem.querySelector('.card__image');
+    const cardTitle = cardItem.querySelector('.card__title');
+    const deleteButton = cardItem.querySelector('.card__delete-button');
+    cardImage.src=item.link;
+    cardImage.alt=`Изображение места: ${item.name}`;
+    cardTitle.textContent=item.name;
+    deleteButton.addEventListener('click', handleDelete);
+
+    return cardItem;
 };
 
 function deleteCard(event) {
@@ -27,7 +21,8 @@ function deleteCard(event) {
 
 function createGallery(data){
     data.forEach(item => {
-        createCard(item);
+        const element = createCard(item, deleteCard);
+        cardsContainer.append(element);
     });
 }
 
